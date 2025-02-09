39613c2ce9c9da7445fb7105a0ee4eb147598b4b
Luke <lukethomasyig@gmail.com>
2025-02-09 02:02:53 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module4Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declare the constants
+            const double PRICE_ONE = 39.95;
+            const double PRICE_TWO = 89.99;
+            const double PRICE_THREE = 139.95;
+            const int SIZE_ONE = 5;
+            const int SIZE_TWO = 8;
+            //Declare the variables
+            double hieghtInFeet;
+            double finalPrice = 0;
+
+            hieghtInFeet = Convert.ToDouble(textBox1.Text);
+            //Decide what the outcome is using a nested decision
+            if (hieghtInFeet <= SIZE_TWO)
+                if (hieghtInFeet >= SIZE_ONE)
+                    finalPrice = PRICE_TWO;
+                else if (hieghtInFeet > SIZE_TWO)
+                    finalPrice = PRICE_THREE;
+                else if (hieghtInFeet < SIZE_ONE)
+                        finalPrice = PRICE_ONE;
+                
+            //Display the output
+            label2.Text = $"It will cost ${finalPrice} for a sign that is {hieghtInFeet} feet long.";
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Module4Project")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Razer")]
+[assembly: AssemblyProduct("Module4Project")]
+[assembly: AssemblyCopyright("Copyright © Razer 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2ee9b9c2-e049-4b0f-a47d-1f273e420b91")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
